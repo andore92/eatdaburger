058f259d5949c157665739677e6aa7352bac70d1
Andrew Sadowski <Andrew Sadowski>
2017-07-05 10:06:38 -0400

    set up schema file for burgers_db along with the seed schema for populating the db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Goodburger Burger', false);
